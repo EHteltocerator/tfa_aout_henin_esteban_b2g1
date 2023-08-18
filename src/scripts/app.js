3ed62f60c326be99c95e53e08e512cde8bbc933e
Telto <ehteltocerator@hotmail.fr>
2023-08-18 11:35:43 +0200

    création de la page uikit et correction js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,56 +5,60 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-var titres = document.querySelectorAll("h2");
-var navLinks = document.querySelectorAll(".nav__link");
-var navBtn = document.querySelector(".nav__btn");
-var nav = document.querySelector(".nav");
+    var navBtn = document.querySelector(".nav__btn");
+    var nav = document.querySelector(".nav");
+    var titres = document.querySelectorAll("h2");
+    var navLinks = document.querySelectorAll(".nav__link");
 
     navBtn.addEventListener("click", (e) => {
         nav.classList.toggle("nav--open");
     })
-    
+
     navLinks.forEach((link) => {
         link.addEventListener("click", (e) => {
             nav.classList.remove("nav--open");
         })
     });
+    
+    titres.forEach((titre) => {
+        gsap.from(titre, { duration: .2, opacity: 0, x: -100, scrollTrigger: { trigger: titre, start: "bottom bottom" } });
+    });
 
-titres.forEach((titre) => {
-    gsap.from(titre, {duration: .2, opacity: 0, x: -100, scrollTrigger: {trigger: titre, start: "bottom bottom"}});
-});
+    for (let i = 0; i < titres.length - 1; i++) {
+        ScrollTrigger.create({
+            trigger: titres[i], start: "top bottom",
 
-for (let i = 0; i < titres.length-1; i++) {
-    ScrollTrigger.create({trigger: titres[i], start: "top bottom",
+            onEnter: (e) => {
+                navLinks.forEach((e) => {
+                    e.classList.remove("nav__link--active");
+                });
+                navLinks[i].classList.add("nav__link--active");
+            },
 
-    onEnter: (e) => {
-        navLinks.forEach((e) => {
-            e.classList.remove("nav__link--active");
-        });
-        navLinks[i].classList.add("nav__link--active");
-    },
+            onEnterBack: (e) => {
+                navLinks.forEach((e) => {
+                    e.classList.remove("nav__link--active");
+                });
+                navLinks[i].classList.add("nav__link--active");
+            },
 
-    onEnterBack: (e) => {
-        navLinks.forEach((e) => {
-            e.classList.remove("nav__link--active");
-        });
-        navLinks[i].classList.add("nav__link--active");
-    },
+            onLeaveBack: (e) => {
+                navLinks.forEach((e) => {
+                    e.classList.remove("nav__link--active");
+                });
 
-    onLeaveBack: (e) => {
-        navLinks.forEach((e) => {
-            e.classList.remove("nav__link--active");
-        });
+                if (!i == 0) {
+                    navLinks[i - 1].classList.add("nav__link--active");
+                }
+            }
 
-        if (!i == 0) {
-            navLinks[i-1].classList.add("nav__link--active");
-        }
-    }
+        })
+    };
 
-})
-};
+    var head = document.querySelector("h1");
 
-var head = document.querySelector("h1");
+    gsap.from(head, { duration: 1, opacity: 0 });
 
-gsap.from(head, {duration: 1, opacity: 0});
+
+
 
