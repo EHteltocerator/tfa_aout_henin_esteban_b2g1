7fcb8b7a5984abb0655584178ff50687a46974d6
Telto <ehteltocerator@hotmail.fr>
2023-08-18 05:31:37 +0200

    ajout de css sur le menu + scrolltrigger pour ajouter la class activ sur les links de la nav en fonction du scroll

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,10 +8,31 @@ gsap.registerPlugin(ScrollTrigger);
 console.log(gsap.version);
 
 var titres = document.querySelectorAll("h2");
+var navLinks = document.querySelectorAll(".nav__link");
 
 titres.forEach((titre) => {
     gsap.from(titre, {duration: .2, opacity: 0, x: -100, scrollTrigger: {trigger: titre, start: "bottom bottom"}});
 });
+
+for (let i = 0; i < titres.length; i++) {
+    ScrollTrigger.create({trigger: titres[i], start: "top bottom",
+
+    onEnter: (e) => {
+        navLinks.forEach((e) => {
+            e.classList.remove("nav__link--active");
+        });
+        navLinks[i].classList.add("nav__link--active");
+    },
+
+    onEnterBack: (e) => {
+        navLinks.forEach((e) => {
+            e.classList.remove("nav__link--active");
+        });
+        navLinks[i].classList.add("nav__link--active");
+    }
+
+})
+};
 
 var head = document.querySelector("h1");
 
